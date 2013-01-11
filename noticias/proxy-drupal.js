e01f95623d9967f3b949ef5bba9d7f72d54d4564
marcio <mgalli@telasocial.com>
2013-01-11 17:40:54 -0200

    Drupal integration

--- noticias/proxy-drupal.js
+++ noticias/proxy-drupal.js
@@ -1,17 +1,20 @@
 
-doFilter = function () { 
+doFilter = function (that) { 
 
- var title   = $(this).find('title').text();
- var link = $(this).find('description').text();
+ var title   = $(that).find('title').text();
+ var link = $(that).find('description').text();
  $('#temp').html(link);
  var plainDesc = $('#temp').text();	
  var desc = plainDesc.split('slide')[0];
  var lines = desc.split('\n');
  desc = lines[0].split('Body:')[1]; 
+
  var descFull='';
+
  for(var k=1; k<lines.length;k++) { 
 	descFull+=lines[k];
  }   
+
  var slides = plainDesc.split('slide')[1];
  var src = 'http'+slides.split('http')[1];
 	
