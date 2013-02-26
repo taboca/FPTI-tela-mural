be4b5bd19d0e3166f68ce9e81846e089c46faf6e
marcio <mgalli@telasocial.com>
2013-02-26 10:40:33 -0300

    mini

--- scripts/screenshots/index.sh
+++ scripts/screenshots/index.sh
@@ -3,5 +3,7 @@ echo 'screenshot taking'
 export DISPLAY=:0.0
 /usr/bin/xwd -root -out file.xwd
 convert file.xwd current.jpg
+convert current.jpg -resize 320 mini.jpg
 cp current.jpg ./app/static
+cp mini.jpg ./app/static
 
